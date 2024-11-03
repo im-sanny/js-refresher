05094f37ff9cf0ca10af12086150ee4a31ad7ea6
im-sanny <raselparvezsanny@gmail.com>
2024-11-03 19:09:58 +0600

    add: event handler added

---
+++ eventHandler.js
@@ -0,0 +1,10 @@
+//select button
+const button = document.getElementById("button");
+
+//event handler
+function hello(){
+    console.log('hehe boi')
+}
+
+button.addEventListener('click', hello)
+
